d9d0755aff83b09e4de463ab2c7ce71241de9706
uppfinnarn <uppfinnarn@gmail.com>
2016-04-29 20:30:28 +0200

    This should definitely be a function

--- src/cli/main.c
+++ src/cli/main.c
@@ -33,17 +33,8 @@ void print_usage(const char *pname) {
 	fprintf(stderr, "        Frame size, can be 960 (20ms), 1920 (40ms) or 2880 (60ms)\n");
 }
 
-int main(int argc, char **argv) {
+int make_output_context(config_t *config, AVCodecContext **out_ctx) {
 	int err = 0;
-
-	config_t *config = malloc(sizeof(config_t));
-	config_defaults(config);
-	if ((err = parse_args(config, argc, argv) || config->infile == NULL)) {
-		print_usage(argv[0]);
-		return err;
-	}
-
-	avcodec_register_all();
 
 	AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_PCM_S16LE);
 	if (!codec) {
@@ -60,6 +51,25 @@ int main(int argc, char **argv) {
 	if ((err = avcodec_open2(ctx, codec, NULL)) < 0) {
 		fprintf(stderr, "Couldn't open codec: %s\n", get_av_err_str(err));
 		return 1;
+	}
+
+	*out_ctx = ctx;
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	config_t *config = malloc(sizeof(config_t));
+	config_defaults(config);
+	if ((err = parse_args(config, argc, argv) || config->infile == NULL)) {
+		print_usage(argv[0]);
+		return err;
+	}
+
+	avcodec_register_all();
+
+	AVCodecContext *octx;
+	if (make_output_context(config, &octx)) {
+		return err;
 	}
 
 	return 0;
