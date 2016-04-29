5d4bd7773f024bcbf1e95e18ac46c6343111986d
uppfinnarn <uppfinnarn@gmail.com>
2016-04-29 20:25:32 +0200

    I can get an output codec, I guess

--- src/cli/main.c
+++ src/cli/main.c
@@ -1,28 +1,23 @@
 #include "config.h"
 #include <dca/dca.h>
+#include <libavformat/avformat.h>
 #include <string.h>
 #include <stdio.h>
 
 /**
- * Prints a usage message to stderr.
+ * Returns a static error string for a libav error.
  */
-void print_usage(const char *pname);
-
-int main(int argc, char **argv) {
-	config_t config;
-	config_defaults(&config);
-	if (parse_args(&config, argc, argv) || config.infile == NULL) {
-		print_usage(argv[0]);
-		return 1;
+const char* get_av_err_str(const int err) {
+	static char buf[1024];
+	if (av_strerror(err, buf, sizeof(buf))) {
+		strcpy(buf, "[COULDN'T GET A PROPER ERROR]");
 	}
-
-	printf("OPUS Mode: %d\n", config.opus_mode);
-
-	dca_t *dca = dca_new(DCA_VERSION);
-	printf("DCA version: %d\n", dca_version(dca));
-	dca_free(dca);
+	return buf;
 }
 
+/**
+ * Prints a usage message to stderr.
+ */
 void print_usage(const char *pname) {
 	fprintf(stderr, "Usage: %s [flags] input\n", pname);
 	fprintf(stderr, "Flags:\n");
@@ -36,4 +31,36 @@ void print_usage(const char *pname) {
 	fprintf(stderr, "        Audio sample rate\n");
 	fprintf(stderr, "    -as ... [default: 960]\n");
 	fprintf(stderr, "        Frame size, can be 960 (20ms), 1920 (40ms) or 2880 (60ms)\n");
+}
+
+int main(int argc, char **argv) {
+	int err = 0;
+
+	config_t *config = malloc(sizeof(config_t));
+	config_defaults(config);
+	if ((err = parse_args(config, argc, argv) || config->infile == NULL)) {
+		print_usage(argv[0]);
+		return err;
+	}
+
+	avcodec_register_all();
+
+	AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_PCM_S16LE);
+	if (!codec) {
+		fprintf(stderr, "The s16le codec doesn't exist!?\n");
+		return 1;
+	}
+
+	AVCodecContext *ctx = avcodec_alloc_context3(codec);
+	ctx->sample_fmt = AV_SAMPLE_FMT_S16;
+	ctx->bit_rate = config->bit_rate * 1000;
+	ctx->sample_rate = config->sample_rate;
+	ctx->channel_layout = av_get_default_channel_layout(config->channels);
+
+	if ((err = avcodec_open2(ctx, codec, NULL)) < 0) {
+		fprintf(stderr, "Couldn't open codec: %s\n", get_av_err_str(err));
+		return 1;
+	}
+
+	return 0;
 }
