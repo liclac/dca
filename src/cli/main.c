2d0863f2e46d4224101a539d14935a0cd9bc5ecd
uppfinnarn <uppfinnarn@gmail.com>
2016-04-30 13:20:38 +0200

    This seems to produce valid output

--- src/cli/main.c
+++ src/cli/main.c
@@ -65,6 +65,8 @@ int make_output_context(config_t *config, AVCodecContext **out_ctx) {
 }
 
 int main(int argc, char **argv) {
+	int err;
+
 	config_t *config = malloc(sizeof(config_t));
 	config_defaults(config);
 	if ((err = parse_args(config, argc, argv) || config->infile == NULL)) {
@@ -73,11 +75,88 @@ int main(int argc, char **argv) {
 	}
 
 	avcodec_register_all();
+	av_register_all();
 
 	AVCodecContext *octx;
 	if (make_output_context(config, &octx)) {
 		return err;
 	}
+
+	AVFormatContext *ifctx;
+	if ((err = avformat_open_input(&ifctx, config->infile, NULL, NULL)) < 0) {
+		fprintf(stderr, "Couldn't open input: %s\n", get_av_err_str(err));
+		return err;
+	}
+	if ((err = avformat_find_stream_info(ifctx, NULL)) < 0) {
+		fprintf(stderr, "Couldn't find stream info: %s\n", get_av_err_str(err));
+		avformat_close_input(&ifctx);
+		return err;
+	}
+
+	AVCodec *icodec;
+	int stream_id = av_find_best_stream(ifctx, AVMEDIA_TYPE_AUDIO, -1, -1, &icodec, 0);
+	if (stream_id == AVERROR_STREAM_NOT_FOUND) {
+		fprintf(stderr, "Couldn't find an audio stream\n");
+		avformat_close_input(&ifctx);
+		return AVERROR_STREAM_NOT_FOUND;
+	}
+	if (stream_id == AVERROR_DECODER_NOT_FOUND) {
+		fprintf(stderr, "Can't find a decoder for any audio stream\n");
+		avformat_close_input(&ifctx);
+		return AVERROR_DECODER_NOT_FOUND;
+	}
+	AVStream *stream = ifctx->streams[stream_id];
+	AVCodecContext *ictx = stream->codec;
+
+	if ((err = avcodec_open2(ictx, icodec, NULL)) < 0) {
+		fprintf(stderr, "Can't open codec: %s\n", get_av_err_str(err));
+		avformat_close_input(&ifctx);
+		return err;
+	}
+
+	AVPacket pkt;
+	AVFrame *frame = av_frame_alloc();
+	while (1) {
+		if ((err = av_read_frame(ifctx, &pkt)) < 0) {
+			if (err != AVERROR_EOF) {
+				fprintf(stderr, "%s\n", get_av_err_str(err));
+			}
+			av_packet_unref(&pkt);
+			break;
+		}
+
+		if (pkt.stream_index != stream_id) {
+			av_packet_unref(&pkt);
+			continue;
+		}
+
+		int got_frame;
+		if ((err = avcodec_decode_audio4(ictx, frame, &got_frame, &pkt)) < 0) {
+			fprintf(stderr, "Couldn't decode audio: %s\n", get_av_err_str(err));
+			av_packet_unref(&pkt);
+			break;
+		}
+
+		av_packet_unref(&pkt);
+
+		if (got_frame) {
+			int data_size = av_samples_get_buffer_size(NULL, ictx->channels, frame->nb_samples, ictx->sample_fmt, 1);
+			fwrite(frame->data[0], 1, data_size, stdout);
+
+			// int got_pkt;
+			// if ((err = avcodec_encode_audio2(octx, &pkt, frame, &got_pkt)) < 0) {
+			// 	fprintf(stderr, "Couldn't encode audio: %s\n", get_av_err_str(err));
+			// 	av_frame_unref(frame);
+			// 	break;
+			// }
+
+			// if (got_pkt) {
+
+			// }
+		}
+	}
+
+	av_frame_free(&frame);
 
 	return 0;
 }
