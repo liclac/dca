220b6b20ceeb0baeab1f1ba89c2d021251d3c554
uppfinnarn <uppfinnarn@gmail.com>
2016-04-30 19:29:49 +0200

    This doesn't need a function

--- src/cli/main.c
+++ src/cli/main.c
@@ -33,37 +33,6 @@ void print_usage(const char *pname) {
 	fprintf(stderr, "        Frame size, can be 960 (20ms), 1920 (40ms) or 2880 (60ms)\n");
 }
 
-/**
- * Makes an output context.
- *
- * @param  config  Program configuration
- * @param  out_ctx Populated with a context on success
- * @return         0 on success
- */
-int make_output_context(config_t *config, AVCodecContext **out_ctx) {
-	int err = 0;
-
-	AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_PCM_S16LE);
-	if (!codec) {
-		fprintf(stderr, "The s16le codec doesn't exist!?\n");
-		return 1;
-	}
-
-	AVCodecContext *ctx = avcodec_alloc_context3(codec);
-	ctx->sample_fmt = AV_SAMPLE_FMT_S16;
-	ctx->bit_rate = config->bit_rate * 1000;
-	ctx->sample_rate = config->sample_rate;
-	ctx->channel_layout = av_get_default_channel_layout(config->channels);
-
-	if ((err = avcodec_open2(ctx, codec, NULL)) < 0) {
-		fprintf(stderr, "Couldn't open codec: %s\n", get_av_err_str(err));
-		return 1;
-	}
-
-	*out_ctx = ctx;
-	return 0;
-}
-
 int main(int argc, char **argv) {
 	int err;
 
@@ -76,11 +45,6 @@ int main(int argc, char **argv) {
 
 	avcodec_register_all();
 	av_register_all();
-
-	AVCodecContext *octx;
-	if (make_output_context(config, &octx)) {
-		return err;
-	}
 
 	AVFormatContext *fctx;
 	if ((err = avformat_open_input(&fctx, config->infile, NULL, NULL)) < 0) {
@@ -112,6 +76,23 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "Can't open codec: %s\n", get_av_err_str(err));
 		avformat_close_input(&fctx);
 		return err;
+	}
+
+	AVCodec *ocodec = avcodec_find_encoder(AV_CODEC_ID_PCM_S16LE);
+	if (!codec) {
+		fprintf(stderr, "The s16le codec doesn't exist!?\n");
+		return 1;
+	}
+
+	AVCodecContext *octx = avcodec_alloc_context3(ocodec);
+	octx->sample_fmt = AV_SAMPLE_FMT_S16;
+	octx->bit_rate = config->bit_rate * 1000;
+	octx->sample_rate = config->sample_rate;
+	octx->channel_layout = av_get_default_channel_layout(config->channels);
+
+	if ((err = avcodec_open2(octx, ocodec, NULL)) < 0) {
+		fprintf(stderr, "Couldn't open codec: %s\n", get_av_err_str(err));
+		return 1;
 	}
 
 	AVPacket pkt;
